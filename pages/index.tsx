cd6ce723076acff139007eccb08ae5dc3f24e97c
sam <3161728@gmail.com>
2021-02-04 11:29:14 +0900

    Install styled reset and add global style on _app component

--- pages/index.tsx
+++ pages/index.tsx
@@ -3,9 +3,22 @@ import { NextPage } from "next";
 import styled from "styled-components";
 
 const Container = styled.div`
-  font-style: italic;
+  padding: 20px;
 `;
 
-const index: NextPage = () => <Container>Hello Typescript!</Container>;
+const index: NextPage = () => {
+  return (
+    <Container>
+      <h1>hello styled-components</h1>
+      <h2>hello styled-components</h2>
+      <p>hello styled-components</p>
+      <ul>
+        <li>hello styled-components</li>
+      </ul>
+      <a>hello styled-components</a>
+      <span>hello styled-components</span>
+    </Container>
+  );
+};
 
 export default index;
