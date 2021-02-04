04cf5de67ff88c114b63dbb7d42aa56d9ea73582
sam <3161728@gmail.com>
2021-02-04 11:19:34 +0900

    Install styled-components and apply SSR on _document.tsx and babel

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,11 @@
 import React from "react";
 import { NextPage } from "next";
+import styled from "styled-components";
 
-const index: NextPage = () => <div>Hello Typescript!</div>;
+const Container = styled.div`
+  font-style: italic;
+`;
+
+const index: NextPage = () => <Container>Hello Typescript!</Container>;
 
 export default index;
