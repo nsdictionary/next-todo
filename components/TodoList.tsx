717e0e3ad842604bee13504dee8dce8df39d20cd
sam <3161728@gmail.com>
2021-02-04 14:50:09 +0900

    Add todo item right side content and style

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -2,6 +2,8 @@ import React, { useCallback, useMemo } from "react";
 import styled from "styled-components";
 import { TodoType } from "../types/todo";
 import palette from "../styles/palette";
+import TrashCanIcon from "../public/static/svg/trash_can.svg";
+import CheckMarkIcon from "../public/static/svg/check_mark.svg";
 
 const Container = styled.div`
   width: 100%;
@@ -97,6 +99,25 @@ const Container = styled.div`
           font-size: 16px;
         }
       }
+
+      .todo-right-side {
+        display: flex;
+        margin-right: 12px;
+        svg {
+          &:first-child {
+            margin-right: 16px;
+          }
+        }
+
+        .todo-button {
+          width: 20px;
+          height: 20px;
+          border-radius: 50%;
+          border: 1px solid ${palette.gray};
+          background-color: transparent;
+          outline: none;
+        }
+      }
     }
   }
 `;
@@ -169,6 +190,24 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
               >
                 {todo.text}
               </p>
+            </div>
+            <div className="todo-right-side">
+              {todo.checked && (
+                <>
+                  <TrashCanIcon className="todo-trash-can" onClick={() => {}} />
+                  <CheckMarkIcon
+                    className="todo-check-mark"
+                    onClick={() => {}}
+                  />
+                </>
+              )}
+              {!todo.checked && (
+                <button
+                  type="button"
+                  className="todo-button"
+                  onClick={() => {}}
+                />
+              )}
             </div>
           </li>
         ))}
