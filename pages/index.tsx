be2b9fa4165c524dc055ee1054c6995fdd105300
sam <3161728@gmail.com>
2021-02-04 15:30:50 +0900

    Add api libs

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,18 +1,25 @@
 import React from "react";
-import { NextPage } from "next";
+import { GetServerSideProps, NextPage } from "next";
 import TodoList from "../components/TodoList";
 import { TodoType } from "../types/todo";
+import { getTodosAPI } from "../lib/api/todo";
 
-const todos: TodoType[] = [
-  { id: 5, text: "요리 연습하기", color: "blue", checked: false },
-  { id: 6, text: "분리수거 하기", color: "navy", checked: false },
-  { id: 7, text: "마트가서 장보기", color: "red", checked: true },
-  { id: 8, text: "마트가서 장보기", color: "red", checked: true },
-  { id: 9, text: "마트가서 장보기", color: "blue", checked: false },
-];
+interface IProps {
+  todos: TodoType[];
+}
 
-const index: NextPage = () => {
+const app: NextPage<IProps> = ({ todos }) => {
   return <TodoList todos={todos} />;
 };
 
-export default index;
+export const getServerSideProps: GetServerSideProps = async () => {
+  try {
+    const { data } = await getTodosAPI();
+    return { props: { todos: data } };
+  } catch (e) {
+    console.log(e);
+    return { props: { todos: [] } };
+  }
+};
+
+export default app;
