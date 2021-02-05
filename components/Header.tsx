0b01e7826f56634fff4a1da82daca93d6625c1ea
sam <3161728@gmail.com>
2021-02-05 14:00:13 +0900

    Apply tailwind css on add todo page

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ import styled from "styled-components";
 
 const Container = styled.div.attrs({
   className:
-    "flex border-b border-gray-200 border-solid text-2xl items-center w-full h-11 py-0 pl-3",
+    "flex border-b border-gray-200 border-solid text-2xl items-center w-full h-12 py-0 pl-3 bg-gray-700 text-white",
 })``;
 
 const Header: React.FC = () => {
