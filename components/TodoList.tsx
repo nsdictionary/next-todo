08095842f12e69b6765663894ee0fd9f105e813e
sam <3161728@gmail.com>
2021-02-05 13:30:41 +0900

    Apply tailwind css on todo list component

--- components/TodoList.tsx
+++ components/TodoList.tsx
@@ -8,119 +8,23 @@ import CheckMarkIcon from "../public/static/svg/check_mark.svg";
 import { checkTodoAPI, deleteTodoAPI } from "../lib/api/todo";
 
 const Container = styled.div`
-  width: 100%;
-
-  .todo-num {
-    margin-left: 12px;
-  }
-
-  .todo-list-header {
-    padding: 12px;
-    position: relative;
-    border-bottom: 1px solid ${palette.gray};
-
-    .todo-list-last-todo {
-      font-size: 14px;
-      margin: 0 0 8px;
-
-      span {
-        margin-left: 12px;
-      }
-    }
-
-    .todo-list-header-colors {
-      display: flex;
-
-      .todo-list-header-color-num {
-        display: flex;
-        margin-right: 8px;
-
-        p {
-          font-size: 14px;
-          line-height: 16px;
-          margin: 0 0 0 6px;
-        }
-
-        .todo-list-header-round-color {
-          width: 16px;
-          height: 16px;
-          border-radius: 50%;
-        }
-      }
-    }
-  }
-
   .bg-blue {
     background-color: ${palette.blue};
   }
-
   .bg-green {
     background-color: ${palette.green};
   }
-
   .bg-navy {
     background-color: ${palette.navy};
   }
-
   .bg-orange {
     background-color: ${palette.orange};
   }
-
   .bg-red {
     background-color: ${palette.red};
   }
-
   .bg-yellow {
     background-color: ${palette.yellow};
-  }
-
-  .todo-list {
-    .todo-item {
-      display: flex;
-      justify-content: space-between;
-      align-items: center;
-      width: 100%;
-      height: 52px;
-      border-bottom: 1px solid ${palette.gray};
-
-      .todo-left-side {
-        width: 100%;
-        height: 100%;
-        display: flex;
-        align-items: center;
-        .todo-color-block {
-          width: 12px;
-          height: 100%;
-        }
-        .checked-todo-text {
-          color: ${palette.gray};
-          text-decoration: line-through;
-        }
-        .todo-text {
-          margin-left: 12px;
-          font-size: 16px;
-        }
-      }
-
-      .todo-right-side {
-        display: flex;
-        margin-right: 12px;
-        svg {
-          &:first-child {
-            margin-right: 16px;
-          }
-        }
-
-        .todo-button {
-          width: 20px;
-          height: 20px;
-          border-radius: 50%;
-          border: 1px solid ${palette.gray};
-          background-color: transparent;
-          outline: none;
-        }
-      }
-    }
   }
 `;
 
@@ -206,42 +110,47 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
   };
 
   return (
-    <Container>
-      <div className="todo-list-header">
-        <p className="todo-list-last-todo">
-          남은 TODO<span>{localTodos.length}개</span>
+    <Container className="w-full">
+      <div className="p-3 relative border-b border-solid border-gray-200">
+        <p className="text-base mx-0 mb-2">
+          남은 TODO<span className="ml-2">{localTodos.length}개</span>
         </p>
-        <div className="todo-list-header-colors">
+        <div className="flex">
           {Object.keys(todoColorNums).map((color, index) => (
-            <div className="todo-list-header-color-num" key={index}>
-              <div className={`todo-list-header-round-color bg-${color}`} />
-              <p>{todoColorNums[color]}개</p>
+            <div className="flex mr-2" key={index}>
+              <div className={`w-4 h-4 rounded-full bg-${color}`} />
+              <p className="text-base leading-4 m-0 ml-1">
+                {todoColorNums[color]}개
+              </p>
             </div>
           ))}
         </div>
       </div>
-      <ul className="todo-list">
+      <ul>
         {localTodos.map((todo) => (
-          <li className="todo-item" key={todo.id}>
-            <div className="todo-left-side">
-              <div className={`todo-color-block bg-${todo.color}`} />
+          <li
+            className="flex justify-between items-center w-full h-12 border-b border-solid border-gray-200"
+            key={todo.id}
+          >
+            <div className="w-full h-full flex items-center">
+              <div className={`w-4 h-full bg-${todo.color}`} />
               <p
-                className={`todo-text ${
-                  todo.checked ? "checked-todo-text" : ""
+                className={`ml-3 text-base ${
+                  todo.checked ? "text-gray-200 line-through" : ""
                 }`}
               >
                 {todo.text}
               </p>
             </div>
-            <div className="todo-right-side">
+            <div className="flex items-center mr-4">
               {todo.checked && (
                 <>
                   <TrashCanIcon
-                    className="todo-trash-can"
+                    className="mr-4 w-4 h-4"
                     onClick={() => deleteTodo(todo.id)}
                   />
                   <CheckMarkIcon
-                    className="todo-check-mark"
+                    className="w-6 h-6"
                     onClick={() => checkTodo(todo.id)}
                   />
                 </>
@@ -249,7 +158,7 @@ const TodoList: React.FC<IProps> = ({ todos }) => {
               {!todo.checked && (
                 <button
                   type="button"
-                  className="todo-button"
+                  className="w-6 h-6 border-2 border-solid border-gray-200 rounded-full bg-transparent outline-none"
                   onClick={() => checkTodo(todo.id)}
                 />
               )}
